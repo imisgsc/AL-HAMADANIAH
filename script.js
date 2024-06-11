90ea52577e4cb0da30fa62cbd4cd8e54ff145be4
imisgsc <simigracia@gmail.com>
2024-06-11 19:40:47 +0530

    removed js

--- script.js
+++ script.js
@@ -27,20 +27,3 @@ function prevSlide() {
 }
 
 setInterval(nextSlide, 1000); // Change slide every 3 seconds
-
-//btns to card
-
-document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('#buttons-container button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', function () {
-            const targetId = this.getAttribute('data-target');
-            const targetElement = document.querySelector(targetId);
-            
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
-});
